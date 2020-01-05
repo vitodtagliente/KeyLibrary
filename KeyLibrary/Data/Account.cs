e7c6949a47946680f5adfa166269c745334f86e8
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2020-01-05 11:51:34 +0100

    project creation

---
+++ KeyLibrary/Data/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyLibrary.Data
+{
+    class Account
+    {
+		public string Service { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public List<string> OptionalUsernames { get; set; } = new List<string>();
+        public List<string> OptionalPasswords { get; set; } = new List<string>();
+    }
+}
---
+++ KeyLibrary/Data/Database.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyLibrary.Data
+{
+    class Database
+    {
+        public string Filename { get; private set; }
+
+        public List<Account> Accounts { get; set; } = new List<Account>();
+
+        public void Clear()
+        {
+            Accounts.Clear();
+        }
+
+        public bool Load(string filename)
+        {
+
+            return true;
+        }
+
+        public bool Save()
+        {
+            return Save(Filename);
+        }
+
+        public bool Save(string filename)
+        {
+
+            return true;
+        }
+    }
+}
